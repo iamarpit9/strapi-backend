14d97931defe38815f578555e2b588aedfe59842
Arpit Gupta <gupta98960@gmail.com>
2024-02-11 18:05:05 +0530

    added cron job

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+    tasks: {
+      '*/14 * * * *': {
+        task: async ({ strapi }) => {
+          try {
+            // Use the native fetch API available in Node.js (v17.5+)
+            // or require('node-fetch') if you're on an older version of Node.js.
+            const response = await fetch('https://patpug-backend.onrender.com/api/blogs?populate=*');
+            const data = await response.text(); // or .json(), etc.
+            console.log('Cron job executed successfully:', data);
+          } catch (error) {
+            console.error('Error executing cron job:', error);
+          }
+        },
+        // options: {
+        //   tz: 'Europe/Paris', // Set the time zone if needed
+        // },
+      },
+    },
+});
